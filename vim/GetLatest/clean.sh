e9f81869df47e58ce2be1c32e5fdf559875aec2f
XonqNopp <xonqnoppp@gmail.com>
2018-11-21 11:56:09 +0100

    [vim] switching to pathogen with submodules

--- vim/GetLatest/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-rm -vf *.zip *.gz *.dist *.vba *.vim
-echo " Only authorized file in this directory:"
-echo "   GetLatestVimScript.dat and $0"
-echo " All others are not back up and have to be removed."
-echo " "
-ls
--- vim/links.vim
+++
@@ -1,3 +0,0 @@
-http://www.vim.org/
-http://vim.wikia.com/
-http://drchip.0sites.net/astronaut/vim/index.html
