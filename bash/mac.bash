3f99d244b623c3e6d84f5196f026c6577327d1f5
XonqNopp <xonqnoppp@gmail.com>
2020-02-03 06:49:26 +0100

    [bin] mac subdir

--- bash/mac.bash
+++ bash/mac.bash
@@ -1,22 +1,25 @@
-## Mac OS stuff ##
-##
+# Mac OS stuff
+
+export PATH="$PATH:$vash/bin/mac"
+
 export ki="/Volumes/$kiName"
-##
+
 alias ls="ls -AG"
 alias du="du -hd 0"
 alias top="top -o cpu"
 alias JET="drutil tray eject"
-##
-##
-## Some fun with voices... ## {{{
+
+
+# Some fun with voices... {{{
 alias givemefun='echo "startCar listening watching playcello macfright testvoices"'
 alias startCar='say xxxchrrrrrrrw'
 alias listening='say -v Whisper I know you are listening'
 alias watching='say -v Ralph I am watching you'
 alias playcello='say -v Cellos "Dum dum dum dum dum dum dum he he he ho ho ho fa lah lah lah lah lah lah fa lah full hoo hoo hoo"'
 alias bazinga='say -v Ralph Bazinga!'
-## Voices are:
-##   Agnes Vicki Victoria
-##   Bruce Fred Ralph
-##   Albert Bad Bells Boing Bubbles Cellos Deranged Good Hysterical Pipe Trinoids Whisper Zarvox
-## }}}
+# Voices are:
+#   Agnes Vicki Victoria
+#   Bruce Fred Ralph
+#   Albert Bad Bells Boing Bubbles Cellos Deranged Good Hysterical Pipe Trinoids Whisper Zarvox
+# }}}
+
