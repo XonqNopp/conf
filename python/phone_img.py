437b7912eecc29b31ec6405cdf390ff3bf306d7c
XonqNopp <xonqnoppp@gmail.com>
2023-12-08 07:21:16 +0100

    [python] phone_img: trying to extract embedded data

--- python/phone_img.py
+++ python/phone_img.py
@@ -6,13 +6,36 @@ import os
 import shutil
 from datetime import datetime
 from argparse import ArgumentParser
+from PIL import Image
 
 
-today = datetime(2023, 11, 20).timestamp()
+today = datetime(2023, 12, 6).timestamp()
 
 
 VID_EXT = ["mov", "mp4", "m4v"]
 DEL_EXT = ["aae"]
+
+
+def get_creation_date_from_exif(file_path):
+    # TODO not fully working
+    return os.stat(file_path).st_birthtime
+
+    try:
+        with Image.open(file_path) as img:
+            # Extract creation date from EXIF data
+            exif_info = img._getexif()
+
+        creation_date = exif_info.get(36867)  # 36867 corresponds to the DateTimeOriginal tag
+
+        if creation_date:
+            # Convert the creation date to a datetime object
+            return datetime.strptime(creation_date, "%Y:%m:%d %H:%M:%S")
+
+    except (AttributeError, KeyError, IndexError, IOError):
+        pass
+    
+    # If not found, use file creation date
+    return os.stat(file_path).st_birthtime
 
 
 def rename_files_with_timestamp(directories: list):
@@ -35,7 +58,6 @@ def rename_files_with_timestamp(directories: list):
                 continue
 
             ext = os.path.splitext(file_path)[1][1:]
-            creation_time = os.stat(file_path).st_birthtime
 
             if ext.lower() in DEL_EXT:
                 print(f'Delete {file_path}')
@@ -46,6 +68,9 @@ def rename_files_with_timestamp(directories: list):
             if ext.lower() in VID_EXT:
                 prefix = 'VID'
 
+            creation_time = get_creation_date_from_exif(file_path)
+            if not isinstance(creation_time, float):
+                creation_time = creation_time.timestamp()
             if creation_time > today:
                 # Wrong data, do not change
                 print(f'Skip {file_path}: wrong data')
@@ -56,6 +81,18 @@ def rename_files_with_timestamp(directories: list):
             new_filename += f'.{ext.lower()}'
 
             new_file_path = os.path.join(directory_path, new_filename)
+
+            i = 0
+            while os.path.exists(new_file_path):
+                i += 1
+                new_filename = f'{prefix}_'
+                new_filename += datetime.fromtimestamp(creation_time).strftime("%Y%m%d_%H%M%S")
+                new_filename += f'_{i}.{ext.lower()}'
+
+                new_file_path = os.path.join(directory_path, new_filename)
+
+            if new_file_path == file_path:
+                continue
 
             shutil.move(file_path, new_file_path)
             print(f'Renamed: {filename} -> {new_filename}')
