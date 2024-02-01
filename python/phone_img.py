43bfdef441f4efa0dbfdbba94c1a0afc2970e06f
XonqNopp <xonqnoppp@gmail.com>
2024-02-01 18:06:42 +0100

    [python] phone img: wip

--- python/phone_img.py
+++ python/phone_img.py
@@ -2,11 +2,14 @@
 """
 Rename files (mainly pictures from smartphone) with timestamp.
 """
+# pylint: disable=[logging-fstring-interpolation]
 import os
 import shutil
+import logging
 from datetime import datetime
 from argparse import ArgumentParser
-from PIL import Image
+from pathlib import Path
+from PIL import Image  # pylint: disable=[import-error]
 
 
 today = datetime(2023, 12, 6).timestamp()
@@ -16,14 +19,17 @@ VID_EXT = ["mov", "mp4", "m4v"]
 DEL_EXT = ["aae"]
 
 
-def get_creation_date_from_exif(file_path):
+def get_creation_date_from_exif(file_path: Path):
+    """
+    Get creation date of the picture from the EXIF data if possible.
+    """
     # TODO not fully working
     return os.stat(file_path).st_birthtime
 
     try:
         with Image.open(file_path) as img:
             # Extract creation date from EXIF data
-            exif_info = img._getexif()
+            exif_info = img._getexif()  # pylint: disable=[protected-access]
 
         creation_date = exif_info.get(36867)  # 36867 corresponds to the DateTimeOriginal tag
 
@@ -33,12 +39,12 @@ def get_creation_date_from_exif(file_path):
 
     except (AttributeError, KeyError, IndexError, IOError):
         pass
-    
+
     # If not found, use file creation date
     return os.stat(file_path).st_birthtime
 
 
-def rename_files_with_timestamp(directories: list):
+def rename_files_with_timestamp(verbose: int, dryrun: bool, directories: list, subdirs: bool):
     """
     Rename the file.
 
@@ -46,22 +52,27 @@ def rename_files_with_timestamp(directories: list):
         directories (list): directories to go through
     """
     for directory_path in directories:
-        for filename in os.listdir(directory_path):
-            file_path = os.path.join(directory_path, filename)
+        directory = Path(directory_path)
+        logging.debug(f'{directory=}')
+
+        for file_path in directory.iterdir():
+            logging.debug(f'{file_path=}')
+            filename = file_path.name
 
             # Check if the path is a file and not a directory
-            if not os.path.isfile(file_path):
+            if not file_path.is_file():
+                logging.debug(f'Skipping directory {file_path}')
                 continue
 
-            if len(filename) > 13:
-                print(f'Skip, already correct: {file_path}')
+            if len(filename) > 13:  # TODO regex
+                logging.warning(f'Skip, already correct: {file_path}')
                 continue
 
-            ext = os.path.splitext(file_path)[1][1:]
+            ext = file_path.suffix[1:]  # remove dot
 
             if ext.lower() in DEL_EXT:
-                print(f'Delete {file_path}')
-                os.unlink(file_path)
+                logging.warning(f'Delete {file_path}')
+                file_path.unlink()
                 continue
 
             prefix = 'IMG'
@@ -71,31 +82,36 @@ def rename_files_with_timestamp(directories: list):
             creation_time = get_creation_date_from_exif(file_path)
             if not isinstance(creation_time, float):
                 creation_time = creation_time.timestamp()
+
             if creation_time > today:
-                # Wrong data, do not change
-                print(f'Skip {file_path}: wrong data')
+                logging.warning(f'Skip {file_path}: wrong data')
                 continue
 
-            new_filename = f'{prefix}_'
-            new_filename += datetime.fromtimestamp(creation_time).strftime("%Y%m%d_%H%M%S")
-            new_filename += f'.{ext.lower()}'
-
-            new_file_path = os.path.join(directory_path, new_filename)
+            timestamp = datetime.fromtimestamp(creation_time).strftime('%Y%m%d_%H%M%S')
+            new_filename = f'{prefix}_{timestamp}.{ext.lower()}'
+            new_file_path = directory / new_filename
 
             i = 0
-            while os.path.exists(new_file_path):
+            while new_file_path.exists():
                 i += 1
-                new_filename = f'{prefix}_'
-                new_filename += datetime.fromtimestamp(creation_time).strftime("%Y%m%d_%H%M%S")
-                new_filename += f'_{i}.{ext.lower()}'
-
-                new_file_path = os.path.join(directory_path, new_filename)
+                new_filename = f'{prefix}_{timestamp}_{i}.{ext.lower()}'
+                new_file_path = directory / new_filename
 
             if new_file_path == file_path:
+                logging.debug(f'Already the correct filename, skip {file_path}')
                 continue
 
-            shutil.move(file_path, new_file_path)
-            print(f'Renamed: {filename} -> {new_filename}')
+            if subdirs:
+                year = datetime.fromtimestamp(creation_time).strftime('%Y')
+                month = datetime.fromtimestamp(creation_time).strftime('%m')
+
+                subdir = directory / year / month
+                subdir.mkdir(parents=True, exist_ok=True)
+
+                new_file_path = subdir / new_filename
+
+            file_path.rename(new_file_path)
+            logging.warning(f'Renamed: {filename} -> {new_filename}')
 
 
 def main():
@@ -103,13 +119,38 @@ def main():
     Main function used when this file is executed.
     """
     parser = ArgumentParser()
+
     parser.add_argument(
-        '--directory',
-        nargs='+',
+        '-v',
+        '--verbose',
+        action='count',
+        default=0,
     )
+
+    parser.add_argument(
+        '-d',
+        '--dryrun',
+        action='store_true',
+        default=False,
+    )
+
+    parser.add_argument(
+        dest='directory',
+        nargs='+',
+        help='Directories to go through',
+    )
+
+    parser.add_argument(
+        '-s',
+        '--subdirs',
+        action='store_true',
+        default=False,
+        help='Move files in subdirectories sorted by months',
+    )
+
     args = parser.parse_args()
 
-    rename_files_with_timestamp(args.directory)
+    rename_files_with_timestamp(args.verbose, args.dryrun, args.directory, args.subdirs)
 
 
 if __name__ == "__main__":
